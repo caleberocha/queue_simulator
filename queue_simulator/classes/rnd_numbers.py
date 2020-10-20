8006a5e2a7d535c89cc52e3b1bcf9b9dac0b58dd
Calebe Rocha <angelo.calebe@edu.pucrs.br>
2020-10-19 22:34:49 -0300

    Logger em RandomNumbers

--- queue_simulator/classes/rnd_numbers.py
+++ queue_simulator/classes/rnd_numbers.py
@@ -2,10 +2,14 @@ from .rnd_generator import RandomGenerator
 from .rnd_numbers_list import RandomNumberList
 from queue import Queue
 from ..decorators import singleton
+from ..logger import logger
 
 
 @singleton
 class RandomNumbers:
+    def __init__(self):
+        self.logger = logger()
+        
     def set_source(self, seed=None, numbers_count=None, numbers=None):
         if seed is not None and numbers_count is not None:
             self.numbers = RandomGenerator(seed, numbers_count)
@@ -15,7 +19,9 @@ class RandomNumbers:
             raise AttributeError("Nenhuma fonte de números aleatórios definida")
 
     def next(self):
-        return self.numbers.next()
+        n = self.numbers.next()
+        self.logger.debug(f"Random obtido: {n}")
+        return n
 
     def size(self):
         return self.numbers.size()
